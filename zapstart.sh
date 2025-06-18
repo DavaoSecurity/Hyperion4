dfdd810e700e2e467f13115947ce3e955b75e613
Davao Cyber Security <nat@davaosecurity.com>
2025-06-18 19:53:24 +0800

    Update zapstart.sh

--- zapstart.sh
+++ zapstart.sh
@@ -7,13 +7,14 @@ sudo docker pull softwaresecurityproject/zap-stable
 sleep 2
 docker run -v $(pwd):/zap/wrk/:rw --user root ghcr.io/zaproxy/zaproxy:stable zap-baseline.py -t $1 -g gen.conf -r zapbase.html
 sleep 2
-
+a=$1
+b=$2
 # zip
 pass=$(openssl rand -base64 6)
 zip --password ${pass} zapbase.zip zapbase.html
 echo " Your password for "$1" zapbase.zip is "${pass}" " | mail -s "Your zapbase.zip Info" $2
 
 # Email Report
-echo " OWASP ZAP Baseline " | mail -s "OWASP ZAP Base Report for "$1" " -A zapbase.zip $2 
+echo " OWASP ZAP Baseline " | mail -s "OWASP ZAP Base Report for "$1" " -A zapbase.zip $2
 
 # docker run -v $(pwd):/zap/wrk/:rw --user root ghcr.io/zaproxy/zaproxy:stable zap-baseline.py -t https://imaginet.com.py -g gen.conf -r testreport.html
